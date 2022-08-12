1d415dc0ed9c31f6635c3ba565d91c52682a2940
Reena-dll <ouz.spy@gmail.com>
2022-08-12 14:38:37 +0300

    12 Ağustos Commit 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FinalProject-Frontend';
+  title: string = 'FinalProject-Frontend';
+  user: string = 'Oğuzhan Sadıkoğlu';
+ 
 }
